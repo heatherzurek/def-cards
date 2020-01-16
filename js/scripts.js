f5dfc362ed355a66954fc7a28f9a3ec0af4d9273
Heather Zurek <heather.zurek@gmail.com>
2020-01-16 10:56:47 -0800

    updated comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  //This line is telling jQuery to look at the HTML document
-  //Then calling the .ready() method on the HTML document
-  //The ready() method is an built in method in jQuery which helps to load the whole page then execute the rest code
+  //This line is telling jQuery to look at the HTML(index.html) document
+  //Then calling the .ready() method on the HTML(index.html) document
+  //The ready() method is an built in method in jQuery which helps to load the whole page then execute the rest of the code
   $("#eventhandler").click(function() {
-    //then you are targeting the id="eventhandler" from index.html
+    //Here you are targeting the id="eventhandler" from index.html
     //we're passing it as an argument to another function. This is called an anonymous callback function. It is a callback function because it is not executed immediately; instead the function is passed in to the jQuery click() function to be executed at a future time - in the case of the .click() function, when that event handler is triggered by a user clicking on any of the selected elements.
     $("#eventhandler-def").fadeToggle();
   });
